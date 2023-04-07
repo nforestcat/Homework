d7eda649199adf4e707ea5a131a83c53db265ed0
김승진 <88011689+nforestcat@users.noreply.github.com>
2023-04-07 16:07:58 +0900

    Update Ex09.java

--- Ex09.java
+++ Ex09.java
@@ -11,12 +11,30 @@ public class Ex09 {
 		String str = sc.nextLine();
 		char gender = str.charAt(7);
 		System.out.println(gender);
-		if (gender == '1' ||gender =='3') {
+		/*if (gender == '1' ||gender =='3') {
 			System.out.println("남자");
 		}	else if (gender == '2' || gender == '4') {
 			System.out.println("여자");
 		}	else {
 			System.out.println("잘못입력하셨습니다.");
+		}*/
+		switch(gender) {
+		case 1: case 3:
+			System.out.println("남자");
+			break;
+		case 2: case 4:
+			System.out.println("여자");
+			break;
+		default:
+			System.out.println("잘못입력하셨습니다.");
+		}
+		char location = str.charAt(8);
+		switch(location) {
+		case 1:
+			System.out.println("서울");
+			break;
+		default:
+			System.out.println("not 서울");
 		}
 		sc.close();
 	}
