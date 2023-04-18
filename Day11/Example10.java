1dfc5334b1f0e31c6b675e113a0918488fcab16d
김승진 <88011689+nforestcat@users.noreply.github.com>
2023-04-18 09:21:35 +0900

    Update Example10.java
    fixed error

--- Day11/Example10.java
+++ Day11/Example10.java
@@ -27,11 +27,9 @@ public class Example10 {
 		Scanner sc = new Scanner(System.in);
 		int[] a4 = new int[5];
 		for(int i = 0; i<a4.length; i++) {
-			
 			a4[i] = sc.nextInt();
-			
 		}
-		sc.close();
+		
 		
 		for(int i = 0; i<a4.length; i++) {
 			
@@ -40,31 +38,37 @@ public class Example10 {
 		}
 		System.out.println();
 		
-		Scanner sc2 = new Scanner(System.in);
+		//Scanner scan2 = new Scanner(System.in);
 		int[] a5 = new int[5];
 		for(int i = 0; i<a5.length; i++) {
-			/*switch(i) {
+			//a5[i] = sc.nextInt();
+			switch(i) {
 			case 0:
-				a5[i+1] = sc2.nextInt(); 
+				a5[i+1] = sc.nextInt();
+				break;
 			case 1:
-				a5[i-1] = sc2.nextInt();
+				a5[i-1] = sc.nextInt();
+				break;
 			default:
-				a5[i] = sc2.nextInt();
-			}*/
+				a5[i] = sc.nextInt();
+				break;
+			}
 			/*if (i == 0 || i == 1) {
 				if (i == 0) {
-					a5[i+1] = sc2.nextInt();
+					a5[i+1] = sc.nextInt();
 				} else {
-					a5[i-1] = sc2.nextInt();
+					a5[i-1] = sc.nextInt();
 				}
-			}*/
-			a5[i] = sc2.nextInt();
-			
+			}
+			a5[i] = sc.nextInt();*/
 		}
-		sc2.close();
-		int temp = a5[0];
+		
+		sc.close();
+		/*int temp = 0;
+		temp= a5[0];
 		a5[0] = a5[1];
-		a5[1]= temp;
+		a5[1]= temp;*/
+		
 		for(int i = 0; i<a5.length; i++) {
 			
 			System.out.print(a5[i] + "\t");
